198960a2e587412f0d0ebe6ad1cefa1533e8e128
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-05-20 00:04:41 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -2,7 +2,5 @@
 # MNP-Tech prod Deployment
 if [ "$DEPLOYMENT GROUP_NAME" == "bala-dg" ]
 then
-	echo "$DEPLOYMENT GROUP_NAME"
-	cd /var/www/html/
-	sudo cp /alliance-dir/index.html /var/www/html/
-	if
+sudo cp /alliance-dir/index.html /var/www/html/
+if
