fea683559e63fc22e172aac24b96bbb553183f40
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-06-19 22:01:10 +0530

    Update main.tf

--- Terraform/s3-new/main.tf
+++ Terraform/s3-new/main.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket_name
+  bucket = var.Bucket_Name
   acl    = "private"
 
   control_object_ownership = true
