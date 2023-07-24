a70e3b3e7a4ac2e4f87a3b3ba278b836c8e14a24
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-07-24 20:04:54 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deployment
-if [ "$DEPLOYMENT GROUP_NAME" == "mnp-dg" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "mnp-dg" ]
 then
 sudo cp /var/lib/jenkins/workspace/${JOB_NAME}/index.html /var/www/html/
 sudo service httpd restart
