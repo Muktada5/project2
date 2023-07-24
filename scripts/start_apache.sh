8f12490ab032caec30ca027adf5120671aeeb5cd
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-07-24 18:20:52 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # MNP-Tech prod Deployment
-if [ "$DEPLOYMENT GROUP_NAME" == "bala-dg" ]
+if [ "$DEPLOYMENT GROUP_NAME" == "mnp-dg" ]
 then
 sudo cp /var/lib/jenkins/workspace/codedeploy/index.html /var/www/html/
 sudo service httpd restart
