2fb3bc2c39bc964546a89a3a2aabcb5f6b1c6ceb
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-07-24 18:24:04 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -2,6 +2,6 @@
 # MNP-Tech prod Deployment
 if [ "$DEPLOYMENT GROUP_NAME" == "mnp-dg" ]
 then
-sudo cp /var/lib/jenkins/workspace/codedeploy/index.html /var/www/html/
+sudo cp /var/lib/jenkins/workspace/${JOB_NAME}/index.html /var/www/html/
 sudo service httpd restart
 fi
