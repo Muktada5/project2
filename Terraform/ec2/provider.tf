7bed9907456993d33077748d2a63c18876ba6388
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-08-09 00:25:01 +0530

    Create provider.tf

---
+++ Terraform/ec2/provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+   region = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket    = "my-s3-mnp-bucket"
+    key       = "MNP-Tech/modules/ec2-bck/terraform.tfstate"
+    region    = "us-east-1"
+    encrypt   = true
+  }
+}
