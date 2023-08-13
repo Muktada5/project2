52dd2d4c253cd0e1d127b2a6ac41d76b41f65b09
Devopsawsr <133389232+Devopsawsr@users.noreply.github.com>
2023-08-13 12:50:10 +0530

    Update start_apache.sh

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Deployment
-if [ "$DEPLOYMENT_GROUP_NAME" == "mnp-prod-dg" ]
+if [ "$DEPLOYMENT_GROUP_NAME" == "mnp-dev-prod-app-dg" ]
 then
-sudo cp /var/lib/jenkins/workspace/${JOB_NAME}/index.html /var/www/html/
+sudo cp /var/www/html/alliance-dir/index.html /var/www/html/
 sudo service httpd restart
 fi
